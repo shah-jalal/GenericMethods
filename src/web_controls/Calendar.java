f8d99d78ce2a95dbfb32f9d8b72d2acf4988f7a1
Shah-jalal <shah7097tn@gmail.com>
2018-02-12 13:10:51 -0600

    changed modifier

--- src/web_controls/Calendar.java
+++ src/web_controls/Calendar.java
@@ -8,7 +8,7 @@ public class Calendar {
 	 * @param element
 	 * @param dateVal
 	 */
-	public static void selectDateByJS(WebDriver driver, WebElement element, String dateVal){
+	public void selectDateByJS(WebDriver driver, WebElement element, String dateVal){
     	JavascriptExecutor js = ((JavascriptExecutor) driver);
 		js.executeScript("arguments[0].setAttribute('value','"+dateVal+"');", element);	
 	}
